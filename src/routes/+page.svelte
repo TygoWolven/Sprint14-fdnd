8f9d71cdc03f131a991d0e026d572820682fd98e
Kaan <kaan.kalmioglu@hva.nl>
2024-09-30 19:08:04 +0200

    Fix voor components inladen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+    import Program from '$lib/components/program/Program.svelte';
+
+    export let data
+
+    const {title, subtitle, content, semesters} = data
+
+</script>
+
+<Program {title} {content} />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
