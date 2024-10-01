4dc4de9984aa14eea165038b1e5d48d4f3b7c0cd
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-10-01 12:35:48 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+    import Semesters from '$lib/components/program/Semesters.svelte'
+
+    export let data
+   
+    const {title, subtitle, content, semesters} = data
+</script>
+
+<Semesters {semesters} {subtitle} />
+
 <h1>Welcome to SvelteKit</h1>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
